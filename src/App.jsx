5f92fe5465131e640beb5d881b951a1bc7e53801
renegadec <confidencenyirenda7@gmail.com>
2026-01-22 22:47:31 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <main className='main-container'>
+      <div className='container'>
+
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Die.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Die = () => {
+  return (
+    <div>Die</div>
+  )
+}
+
+export default Die
